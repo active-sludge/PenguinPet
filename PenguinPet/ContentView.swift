24b3c9c434ab4c97c5f36483f54e78eef5e061e8
Can <hcdayan@hotmail.com>
2022-08-19 16:26:29 +0300

    Set session category and activate in AppDelegate.

--- PenguinPet/ContentView.swift
+++ PenguinPet/ContentView.swift
@@ -9,51 +9,51 @@ import SwiftUI
 import AVFoundation
 
 struct ContentView: View {
-
-  var body: some View {
-    NavigationView {
-      GeometryReader { geometry in
-        ZStack {
-          Image("bg")
-            .resizable()
-            .edgesIgnoringSafeArea(.all)
-          VStack {
-            Image("penguin_01")
-              .padding(.top, geometry.size.height * 0.1)
-            ZStack {
-              Image("Overlay")
-                .resizable()
-                .edgesIgnoringSafeArea(.bottom)
-              VStack {
-                Text("00:00:00")
-                  .font(Font.custom("Menlo Bold", size: 24.0))
-                .foregroundColor(Color(red: 0.1, green: 0.28, blue: 0.52))
-                  .padding(.top)
-                Spacer()
-                VStack {
-                  HStack {
-                    Button {
-                      // record audio
-                    } label: {
-                      Image("button-record-inactive")
+    
+    var body: some View {
+        NavigationView {
+            GeometryReader { geometry in
+                ZStack {
+                    Image("bg")
+                        .resizable()
+                        .edgesIgnoringSafeArea(.all)
+                    VStack {
+                        Image("penguin_01")
+                            .padding(.top, geometry.size.height * 0.1)
+                        ZStack {
+                            Image("Overlay")
+                                .resizable()
+                                .edgesIgnoringSafeArea(.bottom)
+                            VStack {
+                                Text("00:00:00")
+                                    .font(Font.custom("Menlo Bold", size: 24.0))
+                                    .foregroundColor(Color(red: 0.1, green: 0.28, blue: 0.52))
+                                    .padding(.top)
+                                Spacer()
+                                VStack {
+                                    HStack {
+                                        Button {
+                                            // record audio
+                                        } label: {
+                                            Image("button-record-inactive")
+                                        }
+                                        Button {
+                                            // play audio
+                                        } label: {
+                                            Image( "button-play-inactive")
+                                        }
+                                    }
+                                    .padding([.leading, .trailing])
+                                }
+                                .padding(.top, 50)
+                                Spacer()
+                            }
+                        }
                     }
-                    Button {
-                      // play audio
-                    } label: {
-                      Image( "button-play-inactive")
-                    }
-                  }
-                  .padding([.leading, .trailing])
                 }
-                .padding(.top, 50)
-                Spacer()
-              }
-            }
-          }
+            }.navigationBarHidden(true)
         }
-      }.navigationBarHidden(true)
     }
-  }
 }
 
 struct ContentView_Previews: PreviewProvider {
